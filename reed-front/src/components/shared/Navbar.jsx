820a55f0319314361ec2763d877016eda3c5c663
Nishat-M <nishat.mohammad19@gmail.comnjfgjfdfd>
2024-06-08 19:49:43 +0600

    inital navbar set up done

--- reed-front/src/components/shared/Navbar.jsx
+++ reed-front/src/components/shared/Navbar.jsx
@@ -1,44 +1,42 @@
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+
 
 const Navbar = () => {
+
+    const navLinks =
+        <>
+            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/'}>About</Link></li>
+            <li><Link to={'/'}>Services</Link></li>
+            <li><Link to={'/'}>Team</Link></li>
+            <li><Link to={'/'}>Contact</Link></li>
+        </>
+        ;
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-base-200">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            {navLinks}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <Link to={'/'} className="flex items-center gap-2"> 
+                    <img className="w-12 md:w-16" src="https://i.ibb.co/GtQ9TJJ/reed-logo.png" alt="" />
+                    <h2 className=" text-sm md:text-lg first-letter:text-xl first-letter:md:text-3xl first-letter:text-yellow-500">Reed <br /> Consultancy</h2>
+                    </Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {navLinks}
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <a className="btn"><FaSearch /></a>
                 </div>
             </div>
         </div>
--- reed-front/src/root/Root.jsx
+++ reed-front/src/root/Root.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/shared/Navbar";
 
 const Root = () => {
     return (
-        <div>
+        <div className="max-w-6xl mx-auto">
             <div>
                 <Navbar></Navbar>
             </div>
