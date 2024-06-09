80a48e23235628c254bdaa35729bae49174c1048
Nishat-M <nishat.mohammad19@gmail.comnjfgjfdfd>
2024-06-09 11:57:26 +0600

    home cardContainer section done with responsive

--- reed-front/src/components/homepage/HomeAbout.jsx
+++ reed-front/src/components/homepage/HomeAbout.jsx
@@ -5,12 +5,12 @@ const HomeAbout = () => {
             <div className="absolute left-0 top-0 w-full h-full bg-black/70">
 
             </div>
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-2/3 space-y-4 text-gray-300">
-                <h4 className="text-4xl text-center mb-8 text-yellow-500">ABOUT US</h4>
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12 md:w-2/3 space-y-4 text-gray-300">
+                <h4 className="text-2xl md:text-4xl text-center mb-8 text-yellow-500">ABOUT US</h4>
 
-                <p className="leading-6 text-justify font-mono text-yellow-100"> <span className="text-yellow-500">Reed Consultancy BD (RCB)</span> is a leading consultancy firm specializing in sustainability and process improvement for the textile industry. RCB was established in 2006 in Dhaka and since then it is supporting textile, apparel and leather industry. We champion Social Responsibility, advocating for a quadruple bottom line: people, planet, profit, and purpose (4P). Our team, with deep expertise in the textile sector, environmental management, and corporate social responsibility, provides tailored solutions for holistic growth.</p>
+                <p className="leading-6 md:text-justify font-mono text-yellow-100"> <span className="text-yellow-500">Reed Consultancy BD (RCB)</span> is a leading consultancy firm specializing in sustainability and process improvement for the textile industry. RCB was established in 2006 in Dhaka and since then it is supporting textile, apparel and leather industry. We champion Social Responsibility, advocating for a quadruple bottom line: people, planet, profit, and purpose (4P). Our team, with deep expertise in the textile sector, environmental management, and corporate social responsibility, provides tailored solutions for holistic growth.</p>
                 
-                <p className="leading-6 text-justify font-mono text-yellow-100">Our mission is to assist textile industries in sustainable growth. We focus on reducing environmental impact, promoting fair labor practices, and enhancing operational efficiency. Our services include implementing eco-friendly practices, sustainable product development, and integrating cutting-edge technologies. By optimizing processes and fostering a culture of quality, we help businesses thrive economically while contributing positively to society and the environment. RCB empowers companies to achieve balanced success, aligning with 4P principles.</p>
+                <p className="leading-6 md:text-justify font-mono text-yellow-100">Our mission is to assist textile industries in sustainable growth. We focus on reducing environmental impact, promoting fair labor practices, and enhancing operational efficiency. Our services include implementing eco-friendly practices, sustainable product development, and integrating cutting-edge technologies. By optimizing processes and fostering a culture of quality, we help businesses thrive economically while contributing positively to society and the environment. RCB empowers companies to achieve balanced success, aligning with 4P principles.</p>
             </div>
         </div>
     );
